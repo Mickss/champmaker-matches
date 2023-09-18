38233d7fbac710788d28bcae435b2ff6b5d27353
Michał Guz <michal.guz.85@gmail.com>
2023-09-18 10:51:20 +0200

    Create schedule for games

--- scr/main/java/org/micks/champmaker/matches/Main.java
+++ scr/main/java/org/micks/champmaker/matches/Main.java
@@ -1,8 +1,12 @@
 package org.micks.champmaker.matches;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(Main.class, args);
     }
 }
