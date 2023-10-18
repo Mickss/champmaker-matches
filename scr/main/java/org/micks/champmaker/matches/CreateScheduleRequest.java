f12d0f433ef4689f3f151c9a45050e53dc7f262d
Michał Guz <michal.guz.85@gmail.com>
2023-10-18 21:50:28 +0200

    Response from main service

--- scr/main/java/org/micks/champmaker/matches/CreateScheduleRequest.java
+++ scr/main/java/org/micks/champmaker/matches/CreateScheduleRequest.java
@@ -1,11 +1,15 @@
 package org.micks.champmaker.matches;
 
+import java.util.List;
+
 public class CreateScheduleRequest {
 
     private Long champId;
+    private List<Object> testList;
 
-    public CreateScheduleRequest(Long champId) {
+    public CreateScheduleRequest(Long champId, List<Object> testList) {
         this.champId = champId;
+        this.testList = testList;
     }
 
     public CreateScheduleRequest() {
@@ -13,5 +17,17 @@ public class CreateScheduleRequest {
 
     public Long getChampId() {
         return champId;
+    }
+
+    public void setChampId(Long champId) {
+        this.champId = champId;
+    }
+
+    public List<Object> getTestList() {
+        return testList;
+    }
+
+    public void setTestList(List<Object> testList) {
+        this.testList = testList;
     }
 }
--- scr/main/java/org/micks/champmaker/matches/GameController.java
+++ scr/main/java/org/micks/champmaker/matches/GameController.java
@@ -11,7 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class GameController {
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void createSchedule(@RequestBody CreateScheduleRequest createScheduleRequest) {
+    public void schedule(@RequestBody CreateScheduleRequest createScheduleRequest) {
         System.out.println("Will create schedule for championship " + createScheduleRequest.getChampId());
+        System.out.println("Sending more " + createScheduleRequest.getTestList().size());
     }
 }
